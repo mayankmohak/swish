edcdfe269f1a6565479795c31584f97398b163a7
mayankmohak <mayank8199@gmail.com>
2021-07-02 14:51:32 +0530

    changes up to cart

--- src/components/Cart/cart.jsx
+++ src/components/Cart/cart.jsx
@@ -2,26 +2,68 @@ import React, { Component } from 'react'
 import "./cart.css"
 
 class Cart extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoaded: false,
+      cart: []
+    }
+  }
+  componentDidMount() {
+    fetch('https://60dc0552c2b6280017feb60a.mockapi.io/cart')
+      .then(res => res.json())
+      .then((cdata) => {
+        this.setState({
+          isLoaded: true,
+          cart: cdata
+        });
+      });
+  }
   render() {
-    return (
-      <React.Fragment>
-        <div className="total-item">Total Item  :  2</div>
-        <div className="continer">
-          <div className="row">
-            <div className="col-md-4 cart-card">
-              Shoes
-            </div>
-            <div className="col-md-4 cart-card">
-              details
-            </div>
-            <div className="col-md-4 cart-card">
-              3
-            </div>
-          </div>
+    if(!this.state.isLoaded)
+      return <h1>Loading...</h1>
+    else {
+      const cartCard = this.state.cart.map( item => 
+        <div className="cart-card">
+          <img className="cart-card-img" src={item.avtarLocation} /><br />
+          <div className="cart-card-name">{item.name}</div><br />
+          <div className="cart-card-detail">{item.detail}</div><br />
+          <div className="cart-card-price">{item.price}</div><br />
+          {item.quantity}
         </div>
-
-      </React.Fragment>
-    );
+        );
+      return (
+        <React.Fragment>
+          <div className="total-item">Total Item  :  2</div>
+          <div className="cart-products-cards">
+            {cartCard}
+          </div>
+          <div className="price-area">
+            <div className="price-sepcifiation">
+              <span className="left">Subtotal</span>
+              <span className="right">2</span>
+            </div>
+            <br />
+            <div className="price-sepcifiation">
+              <span className="left">Shipping & Handling:</span>
+              <span className="right">2</span>
+            </div>
+            <br />
+            <div className="price-sepcifiation">
+              <span className="left">Discount:</span>
+              <span className="right">2</span>
+            </div>
+            <br />
+            <div className="price-sepcifiation">
+              <span className="left">Grand total:</span>
+              <span className="right">2</span>
+            </div>
+            <br />
+            <button className="button">GO TO CHECKOUT</button>
+          </div>
+        </React.Fragment>
+      );
+    }
   }
 }
 
